3cdf16f8372369d2393bb3f2b22a7b78c574e39e
ProdFilthi <155994707+ProdFilthi@users.noreply.github.com>
2025-01-17 22:02:12 +0200

    created the navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-12 px-8 mb-4 items-center h-12 border-b">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+
+      <ul className="flex gap-8">
+        {links.map((link) => (
+          <Link
+            className={`${
+              currentPath === link.label ? "text-zinc-900" : "text-zinc-500"
+            }" text-zinc-500 hover:text-zinc-900 transition-colors"`}
+            key={link.href}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
